2a88a9fd9be9fb86bda4e1fdd2efc49ab9761611
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-07-14 17:21:54 -0300

    finishing-project

--- js/main.js
+++ js/main.js
@@ -6,12 +6,42 @@ let inputBlue = document.getElementById('input-blue');
 let textRed = document.getElementById('text-red');
 let textGreen = document.getElementById('text-green');
 let textBlue = document.getElementById('text-blue');
+let resultArea = document.getElementById('result-area');
 
 function start() {
-  textRed.value = inputRed.value;
-  textGreen.value = inputGreen.value;
-  textBlue.value = inputBlue.value;
+  inputValue();
+  changeColor();
   testRgb();
 }
 
-function testRgb() {}
+function testRgb() {
+  inputRed.addEventListener('input', function () {
+    inputValue();
+    changeColor();
+  });
+  inputGreen.addEventListener('input', function () {
+    inputValue();
+    changeColor();
+  });
+  inputBlue.addEventListener('input', function () {
+    inputValue();
+    changeColor();
+  });
+}
+
+function changeColor() {
+  resultArea.style.backgroundColor =
+    'rgb(' +
+    inputRed.value +
+    ',' +
+    inputGreen.value +
+    ',' +
+    inputBlue.value +
+    ')';
+}
+
+function inputValue() {
+  textRed.value = inputRed.value;
+  textGreen.value = inputGreen.value;
+  textBlue.value = inputBlue.value;
+}
